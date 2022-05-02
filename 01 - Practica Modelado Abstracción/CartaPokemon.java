c737d563c73b1c1a0e8544bdc763b41102a0dd17
Francisco N <86805363+FranciscoNunezH@users.noreply.github.com>
2022-05-02 05:43:02 -0500

    Add files via upload

--- 01 - Practica Modelado Abstracción/CartaPokemon.java
+++ 01 - Practica Modelado Abstracción/CartaPokemon.java
@@ -2,162 +2,38 @@ package Abstraccion;
 
 public class CartaPokemon {
     public static void main(String[] args){
-        shellder();
-        pikachu();
-        raichu();
-        rayquaza();
-        squirtle();
-
+        pokemon("Charizard", "Fuego", "Fase 2", "Ascuas Regias", 2, 100,170, 2, 0, 3);
+        pokemon("Rayquaza", "Normal", "Basico", "Estallido Vigoroso", 3, 120, 130, 2, -30, 2);
+        pokemon("Totodile", "Agua", "Basico", "Golpe de LLuvia", 1, 20, 70, 2, 0, 1);
+        pokemon("Bulbasaur", "Planta", "Basico", "Encontrar Amigo", 1, 0, 60, 2, 0, 1);
+        pokemon("Pikachu", "Electrico", "Basico", "Impacto Estatico", 1, 10, 60, 2, -20, 1);
     }
 
-    public static void shellder(){
-        String nombrePokemon = "Shellder";
-        String tipoPokemon = "Agua";
+    public static void pokemon(String nombre, String tipo, String evo, String atk, int atkCosto, int atkDamage,
+                               int salud, int debilidad, int resistencia, int retirada){
         // Creamos un objeto.
-        Abstraccion pokemonShellder = new Abstraccion();
-        pokemonShellder.setBasicoEvolucion("Basico");
-        pokemonShellder.setPuntosDeSalud(60);
-        pokemonShellder.setNombreAtaque("Placaje");
-        pokemonShellder.setCosteAtaque(1);
-        pokemonShellder.setDanoAtaque(10);
-        pokemonShellder.setDebilidad(2);
-        pokemonShellder.setResistencia(0);
-        pokemonShellder.setCosteRetirada(2);
+        Abstraccion datosPokemon = new Abstraccion();
+        datosPokemon.setBasicoEvolucion(evo);
+        datosPokemon.setPuntosDeSalud(salud);
+        datosPokemon.setNombreAtaque(atk);
+        datosPokemon.setCosteAtaque(atkCosto);
+        datosPokemon.setDanoAtaque(atkDamage);
+        datosPokemon.setDebilidad(debilidad);
+        datosPokemon.setResistencia(resistencia);
+        datosPokemon.setCosteRetirada(retirada);
 
-        String msg = "";
-        msg += "\nEtapa Evolutiva: " + pokemonShellder.getBasicoEvolucion();
-        msg += "\nNombre: " + nombrePokemon;
-        msg += "\nTipo: " + tipoPokemon;
-        msg += "\nPS: " + pokemonShellder.getPuntosDeSalud();
-        msg += "\nNombre Atk: " + pokemonShellder.getNombreAtaque();
-        msg += "\nCoste Atk: " + pokemonShellder.getCosteAtaque();
-        msg += "\nDamage Atk: " + pokemonShellder.getDanoAtaque();
-        msg += "\nDebilidad: Tipo Planta X" + pokemonShellder.getDebilidad();
-        msg += "\nResistencia: " + pokemonShellder.getResistencia();
-        msg += "\nRetirada: " + pokemonShellder.getCosteDeRetirada();
+        String msg = "\nNombre: " + nombre;
+        msg +=  "\nEtapa Evolutiva: " + datosPokemon.getBasicoEvolucion();
+        msg += "\nTipo: " + tipo;
+        msg += "\nPS: " + datosPokemon.getPuntosDeSalud();
+        msg += "\nAtaque: " + datosPokemon.getNombreAtaque();
+        msg += "\n- Costo: " + datosPokemon.getCosteAtaque();
+        msg += "\n- Damage: " + datosPokemon.getDanoAtaque();
+        msg += "\nDebilidad: " + datosPokemon.getDebilidad();
+        msg += "\nResistencia: " + datosPokemon.getResistencia();
+        msg += "\nRetirada: " + datosPokemon.getCosteDeRetirada();
 
-        System.out.print(msg);
-
-    }
-
-    public static void pikachu(){
-        String nombrePokemon = "Pikachu";
-        String tipoPokemon = "Electrico";
-        // Creamos un objeto.
-        Abstraccion pokemonShellder = new Abstraccion();
-        pokemonShellder.setBasicoEvolucion("Basico");
-        pokemonShellder.setPuntosDeSalud(60);
-        pokemonShellder.setNombreAtaque("Impactrueno");
-        pokemonShellder.setCosteAtaque(20);
-        pokemonShellder.setDanoAtaque(20);
-        pokemonShellder.setDebilidad(2);
-        pokemonShellder.setResistencia(-20);
-        pokemonShellder.setCosteRetirada(1);
-
-        String msg = "\n";
-        msg += "\nEtapa Evolutiva: " + pokemonShellder.getBasicoEvolucion();
-        msg += "\nNombre: " + nombrePokemon;
-        msg += "\nTipo: " + tipoPokemon;
-        msg += "\nPS: " + pokemonShellder.getPuntosDeSalud();
-        msg += "\nNombre Atk: " + pokemonShellder.getNombreAtaque();
-        msg += "\nCoste Atk: " + pokemonShellder.getCosteAtaque();
-        msg += "\nDamage Atk: " + pokemonShellder.getDanoAtaque();
-        msg += "\nDebilidad: Tipo Lucha X" + pokemonShellder.getDebilidad();
-        msg += "\nResistencia: Tipo Metal " + pokemonShellder.getResistencia();
-        msg += "\nRetirada: " + pokemonShellder.getCosteDeRetirada();
-
-        System.out.print(msg);
-
-    }
-
-    public static void raichu(){
-        String nombrePokemon = "Raichu";
-        String tipoPokemon = "Electrico";
-        // Creamos un objeto.
-        Abstraccion pokemonShellder = new Abstraccion();
-        pokemonShellder.setBasicoEvolucion("Fase 1");
-        pokemonShellder.setPuntosDeSalud(130);
-        pokemonShellder.setNombreAtaque("Rayo Mach");
-        pokemonShellder.setCosteAtaque(30);
-        pokemonShellder.setDanoAtaque(120);
-        pokemonShellder.setDebilidad(2);
-        pokemonShellder.setResistencia(0);
-        pokemonShellder.setCosteRetirada(1);
-
-        String msg = "\n";
-        msg += "\nEtapa Evolutiva: " + pokemonShellder.getBasicoEvolucion();
-        msg += "\nNombre: " + nombrePokemon;
-        msg += "\nTipo: " + tipoPokemon;
-        msg += "\nPS: " + pokemonShellder.getPuntosDeSalud();
-        msg += "\nNombre Atk: " + pokemonShellder.getNombreAtaque();
-        msg += "\nCoste Atk: " + pokemonShellder.getCosteAtaque();
-        msg += "\nDamage Atk: " + pokemonShellder.getDanoAtaque();
-        msg += "\nDebilidad: Tipo Lucha X" + pokemonShellder.getDebilidad();
-        msg += "\nResistencia: " + pokemonShellder.getResistencia();
-        msg += "\nRetirada: " + pokemonShellder.getCosteDeRetirada();
-
-        System.out.print(msg);
-
-    }
-
-    public static void rayquaza(){
-        String nombrePokemon = "Rayquaza Luminoso";
-        String tipoPokemon = "Dragon";
-        // Creamos un objeto.
-        Abstraccion pokemonShellder = new Abstraccion();
-        pokemonShellder.setBasicoEvolucion("Basico");
-        pokemonShellder.setPuntosDeSalud(120);
-        pokemonShellder.setNombreAtaque("Juicio Celeste");
-        pokemonShellder.setCosteAtaque(4);
-        pokemonShellder.setDanoAtaque(190);
-        pokemonShellder.setDebilidad(2);
-        pokemonShellder.setResistencia(0);
-        pokemonShellder.setCosteRetirada(3);
-
-        String msg = "\n";
-        msg += "\nEtapa Evolutiva: " + pokemonShellder.getBasicoEvolucion();
-        msg += "\nNombre: " + nombrePokemon;
-        msg += "\nTipo: " + tipoPokemon;
-        msg += "\nPS: " + pokemonShellder.getPuntosDeSalud();
-        msg += "\nNombre Atk: " + pokemonShellder.getNombreAtaque();
-        msg += "\nCoste Atk: " + pokemonShellder.getCosteAtaque();
-        msg += "\nDamage Atk: " + pokemonShellder.getDanoAtaque();
-        msg += "\nDebilidad: Tipo Hada X" + pokemonShellder.getDebilidad();
-        msg += "\nResistencia: " + pokemonShellder.getResistencia();
-        msg += "\nRetirada: " + pokemonShellder.getCosteDeRetirada();
-
-        System.out.print(msg);
-
-    }
-
-    public static void squirtle(){
-        String nombrePokemon = "Squirtle";
-        String tipoPokemon = "Agua";
-        // Creamos un objeto.
-        Abstraccion pokemonShellder = new Abstraccion();
-        pokemonShellder.setBasicoEvolucion("Basico");
-        pokemonShellder.setPuntosDeSalud(60);
-        pokemonShellder.setNombreAtaque("Rayo Burbuja");
-        pokemonShellder.setCosteAtaque(10);
-        pokemonShellder.setDanoAtaque(10);
-        pokemonShellder.setDebilidad(2);
-        pokemonShellder.setResistencia(0);
-        pokemonShellder.setCosteRetirada(1);
-
-        String msg = "\n";
-        msg += "\nEtapa Evolutiva: " + pokemonShellder.getBasicoEvolucion();
-        msg += "\nNombre: " + nombrePokemon;
-        msg += "\nTipo: " + tipoPokemon;
-        msg += "\nPS: " + pokemonShellder.getPuntosDeSalud();
-        msg += "\nNombre Atk: " + pokemonShellder.getNombreAtaque();
-        msg += "\nCoste Atk: " + pokemonShellder.getCosteAtaque();
-        msg += "\nDamage Atk: " + pokemonShellder.getDanoAtaque();
-        msg += "\nDebilidad: Tipo Planta X" + pokemonShellder.getDebilidad();
-        msg += "\nResistencia: " + pokemonShellder.getResistencia();
-        msg += "\nRetirada: " + pokemonShellder.getCosteDeRetirada();
-
-        System.out.print(msg);
-
+        System.out.println(msg);
     }
 
 }
